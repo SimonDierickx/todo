3230b10a400e9eec79826ee83f482e4899f14ccf
vagrant <vagrant@dockerlab>
2024-10-22 15:04:33 +0000

    Set permissions

--- todo.sh
+++ todo.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+if ! command -v wget &> /dev/null; then
+  echo "wget could not be found, installing..."
+  apt-get update && apt-get install -y wget
+fi
 wget https://github.com/docker/getting-started/archive/refs/heads/master.zip
 
-apt-get install unzip
+if ! command -v unzip &> /dev/null; then
+  echo "unzip could not be found, installing..."
+  apt-get update && apt-get install -y unzip
+fi
+
 unzip master.zip -d master
+
+if [ ! -d "app" ]; then
+  echo "'app' directory does not exist, ensure the unzipped structure is correct"
+  exit 1
+fi
 
 cd app
 
@@ -15,4 +28,16 @@ echo "RUN yarn install --production" >> Dockerfile
 echo 'CMD ["node", "/app/src/index.js"]' >> Dockerfile
 
 docker build -t todo .
+
+function is_port_in_use {
+  lsof -i :$1 &> /dev/null
+  return $?
+}
+
+PORT=5000
+while is_port_in_use $PORT; do
+  echo "Port $PORT is in use, trying next port..."
+  PORT=$((PORT + 1))
+done
+
 docker run -dp 5000:5000 todo
