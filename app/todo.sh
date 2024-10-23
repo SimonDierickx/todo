4ba0ee0798686e331ff730d484918e699f74313d
SimonDierickx <simon.dierickx@student.hogent.be>
2024-10-23 09:08:28 +0200

    Todo.sh

--- app/todo.sh
+++ app/todo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-touch Dockerfile
+touch todo/app/Dockerfile
 
-cat > Dockerfile << _EOF_
+cat > todo/app/Dockerfile << _EOF_
 FROM node:20-alpine
 RUN apk add --no-cache python3 g++ make
 WORKDIR /app
@@ -10,6 +10,8 @@ COPY  . .
 RUN yarn install --production
 CMD ["node", "/app/src/index.js"]
 _EOF_
+
+cd todo/app
 
 docker build -t todo .
 
