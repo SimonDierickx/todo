63a2b72daf8c2b539f983199f99e543af39c5402
SimonDierickx <simon.dierickx@student.hogent.be>
2024-10-23 08:58:56 +0200

    todo

---
+++ app/todo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+touch Dockerfile
+
+cat > Dockerfile << _EOF_
+FROM node:20-alpine
+RUN apk add --no-cache python3 g++ make
+WORKDIR /app
+COPY  . .
+RUN yarn install --production
+CMD ["node", "/app/src/index.js"]
+_EOF_
+
+docker build -t todo .
+
+function is_port_in_use {
+  lsof -i :$1 &> /dev/null
+  return $?
+}
+
+PORT=2000
+while is_port_in_use $PORT; do
+  echo "Port $PORT is in use, trying next port..."
+  PORT=$((PORT + 1))
+done
+
+echo "The used port is $PORT"
+
+docker run -t -d -p $PORT:$PORT --name todo todoapp
+
+docker ps -a
